28dbea6f29a495c136d7cffb625dfbc67acb8443
Marwen <marwen.meddeb@supinfo.com>
2022-01-14 13:38:47 +0100

    Update Get10Game.py

--- Get10Game.py
+++ Get10Game.py
@@ -1,104 +1,90 @@
-import tkinter as tk
 from tkinter import *
 import tkinter.messagebox as messagebox
 import sys
 import random
 """La classe principale du jeu, qui est le contrôleur de l'ensemble du jeu."""
 
-
 class game:
     def __init__(self, size):
         self.size = size
         self.matrice = self.generate_empty_game()
         self.coord = tuple
-        self.liste = list
+        self.liste= list
         self.current_score = 0
 
     def setCoord(self, coord):
         self.coord = coord
-
+        
     def setList(self, liste):
         self.liste = liste
-
+        
     def generate_empty_game(self):
         return [[0] * size for i in range(size)]
-
+    
     def aleatoire(self):
         for i in range(size):
             for j in range(size):
-                if random.random() < 0.6:
+                if random.random() < 0.4:
                     self.matrice[i][j] = 1
-                elif random.random() < 0.45:
+                elif random.random() < 0.3:
                     self.matrice[i][j] = 2
-                elif random.random() < 0.2:
+                elif random.random() < 0.25:
                     self.matrice[i][j] = 3
-                elif random.random() < 0.1:
+                elif random.random() < 0.05:
                     self.matrice[i][j] = 4
-
+    
     def existe_case_adj(self):  # parametres n ,  matrice a deux dimensions, i et j
         for i in range(size):
             for j in range(size):
-                if not 0 <= i < size or not 0 <= j < size:
-                    return 'Error'
+                if not 0 <= i < size or not 0 <= j < size: return 'Error'
                 if i + 1 < self.size:
-                    if self.matrice[i + 1][j] == self.matrice[i][j]:
-                        return True
+                    if self.matrice[i + 1][j] == self.matrice[i][j]: return True
                 if j + 1 < self.size:
-                    if self.matrice[i][j + 1] == self.matrice[i][j]:
-                        return True
+                    if self.matrice[i][j + 1] == self.matrice[i][j]: return True
                 if 0 <= j - 1:
-                    if self.matrice[i][j - 1] == self.matrice[i][j]:
-                        return True
+                    if self.matrice[i][j - 1] == self.matrice[i][j]: return True
                 if 0 <= i - 1:
-                    if self.matrice[i - 1][j] == self.matrice[i][j]:
-                        return True
+                    if self.matrice[i - 1][j] == self.matrice[i][j]: return True
                 return False
 
     def reste_coup(self):
-        # on debut on a rien trouvé on sait pas si une case a une meme valeur adjacente
-        resultat = False
+        resultat = False  # on debut on a rien trouvé on sait pas si une case a une meme valeur adjacente
         for i in range(size):
             for j in range(size):
                 if self.existe_case_adj():  # si la case a une case adjacente de la meme valeur
                     resultat = True  # le resultat change et devient vrai
         return resultat
 
-    # fonction pour calculer toutes les cases adjacentes d'une cellule
-    def propagation(self, coord: tuple, liste: list):
-        if 0 <= coord[0] < size and 0 <= coord[1] < size:
-            if coord[0] - 1 >= 0:
-                if self.matrice[coord[0] - 1][coord[1]] == self.matrice[coord[0]][coord[1]]:
-                    if (coord[0] - 1, coord[1]) not in liste:
-                        liste.append((coord[0] - 1, coord[1]))
-                        self.propagation(size, self.matrice,
-                                         (coord[0] - 1, coord[1]), liste)
-            if coord[0] + 1 < size:
-                if self.matrice[coord[0] + 1][coord[1]] == self.matrice[coord[0]][coord[1]]:
-                    if (coord[0] + 1, coord[1]) not in liste:
-                        liste.append((coord[0] + 1, coord[1]))
-                        self.propagation(size, self.matrice,
-                                         (coord[0] + 1, coord[1]), liste)
-            if coord[1] - 1 >= 0:
-                if self.matrice[coord[0]][coord[1] - 1] == self.matrice[coord[0]][coord[1]]:
-                    if (coord[0], coord[1] - 1) not in liste:
-                        liste.append((coord[0], coord[1] - 1))
-                        self.propagation(size, self.matrice,
-                                         (coord[0], coord[1] - 1), liste)
-            if coord[1] + 1 < size:
-                if self.matrice[coord[0]][coord[1] + 1] == self.matrice[coord[0]][coord[1]]:
-                    if (coord[0], coord[1] + 1) not in liste:
-                        liste.append((coord[0], coord[1] + 1))
-                        self.propagation(size, self.matrice,
-                                         (coord[0], coord[1] + 1), liste)
+    def propagation(self):   # fonction pour calculer toutes les cases adjacentes d'une cellule
+        if 0 <= self.coord[0] < size and 0 <= self.coord[1] < size:
+            if self.coord[0] - 1 >= 0:
+                if self.matrice[self.coord[0] - 1][self.coord[1]] == self.matrice[self.coord[0]][self.coord[1]]:
+                    if (self.coord[0] - 1, self.coord[1]) not in self.liste:
+                        self.liste.append((self.coord[0] - 1, self.coord[1]))
+                        self.propagation(size, self.matrice, (self.coord[0] - 1, self.coord[1]), self.liste)
+            if self.coord[0] + 1 < size:
+                if self.matrice[self.coord[0] + 1][self.coord[1]] == self.matrice[self.coord[0]][self.coord[1]]:
+                    if (self.coord[0] + 1, self.coord[1]) not in self.liste:
+                        self.liste.append((self.coord[0] + 1, self.coord[1]))
+                        self.propagation(size, self.matrice, (self.coord[0] + 1, self.coord[1]), self.liste)
+            if self.coord[1] - 1 >= 0:
+                if self.matrice[self.coord[0]][self.coord[1] - 1] == self.matrice[self.coord[0]][self.coord[1]]:
+                    if (self.coord[0], self.coord[1] - 1) not in self.liste:
+                        self.liste.append((self.coord[0], self.coord[1] - 1))
+                        self.propagation(size, self.matrice, (self.coord[0], self.coord[1] - 1), self.liste)
+            if self.coord[1] + 1 < size:
+                if self.matrice[self.coord[0]][self.coord[1] + 1] == self.matrice[self.coord[0]][self.coord[1]]:
+                    if (self.coord[0], self.coord[1] + 1) not in self.liste:
+                        self.liste.append((self.coord[0], self.coord[1] + 1))
+                        self.propagation(size, self.matrice, (self.coord[0], self.coord[1] + 1), self.liste)
 
-    # fonction incrementer et pour mettre un 0 sur toutes cases adjacentes qu'on a trouvé
-    def modification(self, liste: list):
+
+    def modification(self , liste: list) : # fonction incrementer et pour mettre un 0 sur toutes cases adjacentes qu'on a trouvé
         self.matrice[liste[0][0]][liste[0][1]] += 1
         for i in range(1, len(liste)):
             self.matrice[liste[i][0]][liste[i][1]] = 0
 
-    # pour deplacer les cases vers le bas s'il ya des 0 et ajouter de nouvelles valeurs aleatoires
-    def gravity(self):
+    def gravity(self) :            # pour deplacer les cases vers le bas s'il ya des 0 et ajouter de nouvelles valeurs aleatoires
         for i in range(size):
             for j in range(size):
                 if self.matrice[i][j] == 0 and i >= 1:
@@ -130,15 +116,15 @@ class game:
         for p in range(size):
             for m in range(size):
                 if self.matrice[p][m] == 0:
-                    # du coup la derniere case en haut n'aura rien et on y met une valeur aleatoire
-                    self.matrice[p][m] = self.aleatoire(self)
-
+                    self.matrice[p][m] = self.aleatoire(self) # du coup la derniere case en haut n'aura rien et on y met une valeur aleatoire
+                    
     def found_10(self):
         for i in range(self.size):
             for j in range(self.size):
                 if self.matrice[i][j] >= 10:
                     return True
         return False
+
 
     def set_matrice(self, matrice):
         self.matrice = matrice
@@ -152,12 +138,10 @@ class game:
 
 """ La classe de vue GUI du jeu Get10 montrant via tkinter """
 
-
-class GUI:
-    MARGIN = 15
+class GUI:                               
     SIZE = 500
     marge_gauche = SIZE / 5      # une marge de gauche pour que ca colle pas a la fenetre
-    marge_haut = SIZE / 5
+    marge_haut = SIZE / 5  
     game_LEN = 4
     game_PADDING = 10
     CELL_PADDING = 5
@@ -197,7 +181,7 @@ class GUI:
         self.window = Tk()
         if sys.platform == 'win32':
             self.window.title('JustGet10')
-
+            
         self.window.resizable(False, False)
         self.background = Frame(self.window, bg=GUI.BACKGROUND_COLOR)
         self.cell_labels = []
@@ -205,53 +189,50 @@ class GUI:
             row_labels = []
             for j in range(self.game.size):
                 label = Label(self.background, text='',
-                              justify=CENTER, font=GUI.FONT,
-                              width=4, height=2)
+                                 justify=CENTER, font=GUI.FONT,
+                                 width=4, height=2)
                 label.grid(row=i, column=j, padx=5, pady=5)
                 row_labels.append(label)
             self.cell_labels.append(row_labels)
         self.background.pack(side=TOP)
         self.start_matrice_num = self.game.size
-        self.window.bind("<Button-1>", self.Leftclick)
+        self.window.bind("<Button-1>" , self.Leftclick)
         self.add_start_matrice()
         self.paint()
         self.window.mainloop()
-
+    
+    
     def paint(self):
         for i in range(self.game.size):
             for j in range(self.game.size):
-                cell_text = str(self.game.matrice[i][j])
-                if self.game.matrice[i][j] > 10:
-                    bg_color = GUI.CELL_BACKGROUND_COLOR_DICT.get('beyond')
-                    fg_color = GUI.CELL_COLOR_DICT.get('beyond')
-                else:
-                    bg_color = GUI.CELL_BACKGROUND_COLOR_DICT.get(cell_text)
-                    fg_color = GUI.CELL_COLOR_DICT.get(cell_text)
-                self.cell_labels[i][j].configure(
-                    text=cell_text,
-                    bg=bg_color, fg=fg_color)
-
+                    cell_text = str(self.game.matrice[i][j])
+                    if self.game.matrice[i][j] > 10:
+                        bg_color = GUI.CELL_BACKGROUND_COLOR_DICT.get('beyond')
+                        fg_color = GUI.CELL_COLOR_DICT.get('beyond')
+                    else:
+                        bg_color = GUI.CELL_BACKGROUND_COLOR_DICT.get(cell_text)
+                        fg_color = GUI.CELL_COLOR_DICT.get(cell_text)
+                    self.cell_labels[i][j].configure(
+                        text=cell_text,
+                        bg=bg_color, fg=fg_color)
+                    
     def add_start_matrice(self):
         for i in range(self.start_matrice_num):
             self.game.aleatoire()
 
+
     def Leftclick(self, event):
         x, y = event.x, event.y
         print('{}, {}'.format(x, y))
-        # calcule la longueur de la grille + sa marge gauche
-        taillex = GUI.MARGIN + (self.game.size * self.label.grid)
-        # calcule la hauteur de la grille + sa marge haut
-        tailley = GUI.MARGIN + (self.game.size * self.label.grid)
-        # test pour connaitre si on a cliqué a l'interieur de la grille
-        if x >= 0 and x < taillex and y >= 0 and y < tailley:
-            # on calcule a partir du y de la souris(qui est en pixel ) le i de la case selectionnée
-            i = int((y - GUI.MARGIN)/self.label.grid)
-            j = int((x - GUI.MARGIN) / self.label.grid)
-            # c'est la case selectionée
-            current = (i, j)
+        taillex = GUI.SIZE + (self.game.size * GUI.CELL_PADDING )  #calcule la longueur de la grille + sa marge gauche
+        tailley = GUI.SIZE + (self.game.size *  GUI.CELL_PADDING)    #calcule la hauteur de la grille + sa marge haut
+        if x >= 0 and x < taillex and y >= 0 and y < tailley :  #test pour connaitre si on a cliqué a l'interieur de la grille
+            i = int((y - GUI.SIZE)/GUI.CELL_PADDING)            # on calcule a partir du y de la souris(qui est en pixel ) le i de la case selectionnée
+            j = int((x - GUI.SIZE) / GUI.CELL_PADDING)  
+            current = (i, j)                                 # c'est la case selectionée
             L = [current]
             print(L)
-        self.game.propagation(self, tuple)
+        self.game.propagation()                
         key_value = event.keysym
         print('{} Mouse Left Click Pressed'.format(key_value))
         self.click()
@@ -261,28 +242,26 @@ class GUI:
             return self.you_win()
 
     def click(self):
-        if self.game.existe_case_adj():
-            # appelle procedure modification pour incrementer et mettre les 0
-            self.game.modification()
-            self.game.gravity()  # appelle procedure gravity
+        if  self.game.existe_case_adj():
+            self.game.modification()    #appelle procedure modification pour incrementer et mettre les 0
+            self.game.gravity()     #appelle procedure gravity  
             self.game.display()
         elif not self.game.reste_coup():
             return self.game_over
-
+    
     def you_win(self):
         if not self.won:
             self.won = True
             print('You Win!')
             if messagebox.askyesno('Get10Game', 'You Win!\n'
-                                   'Are you going to continue the Get10Game game?'):
+                                       'Are you going to continue the Get10Game game?'):
                 self.keep_playing = True
 
     def game_over(self):
         print('Game over!')
         messagebox.showinfo('Get10Game', 'Oops!\n'
-                            'Game over!')
-
-
+                                    'Game over!')
+        
 if __name__ == '__main__':
     size = 6
     game = game(size)
