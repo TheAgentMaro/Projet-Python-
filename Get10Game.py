3722abdbe31cbd06564c60618db5e63c73a3500b
Marwen <marwen.meddeb@supinfo.com>
2022-01-18 13:20:22 +0100

    Update Get10Game.py

--- Get10Game.py
+++ Get10Game.py
@@ -3,18 +3,18 @@ import tkinter.messagebox as messagebox
 import random
 """La classe principale du jeu, qui est le contrôleur de l'ensemble du jeu."""
 
-def aleatoire(proba) :                 #une fonction parce qu'il retourne quelque chose
-    nb = random.random()
-    if(nb < proba[0]):
+def aleatoire(proba) :                 #une fonction parce qu'il retourne les valeurs de façon aleatoire
+    num = random.random()
+    if(num < proba[0]):
         return 4
-    elif(proba[0] < nb < proba[1]):
+    elif(proba[0] < num < proba[1]):
         return 3
-    elif(proba[1] < nb < proba[2]):
+    elif(proba[1] < num < proba[2]):
         return 2
     else:
         return 1
 
-
+#creation du plateau
 def newBoard(n, proba) :                                      
     if(0 < proba[0] < proba[1] < proba[2] < 1):
         plateau = [[0 for i in range(n)] for j in range(n)] 
@@ -31,17 +31,14 @@ class game:
         self.size = size
         self.plateau = newBoard(n , proba)
         print(self.plateau)
-        self.liste2 = []
+        self.liste2 = [0]*size
         self.liste= [0]*size
         self.current_score = self.score()
-
-    def set_plateau(self, plateau):
-        self.plateau = plateau
     
                  
-    def existe_case_adj(self):  # parametres n ,  plateau a deux dimensions, i et j
+    def adjacent(self):  
         for i in range(size):
-            for j in range(size):
+            for j in range(size): # on donne des conditions pour exclure le test sur les cases qui depassent la grille 
                 if not 0 <= i < size or not 0 <= j < size: return 'Error'
                 if i + 1 < self.size:
                     if self.plateau[i + 1][j] == self.plateau[i][j]: return True
@@ -52,33 +49,52 @@ class game:
                 if 0 <= i - 1:
                     if self.plateau[i - 1][j] == self.plateau[i][j]: return True
                 return False
+        #plateau a deux dimensions, i et j
+        # les conditions renvoient True ou False donc la fonction sera de type booleen
 
     def reste_coup(self):
         resultat = False  # on debut on a rien trouvé on sait pas si une case a une meme valeur adjacente
         for i in range(size):
             for j in range(size):
-                if self.existe_case_adj():  # si la case a une case adjacente de la meme valeur
+                if self.adjacent():  # si la case a une case adjacente de la meme valeur
                     resultat = True  # le resultat change et devient vrai
         return resultat
 
     def propagation(self):   # fonction pour calculer toutes les cases adjacentes d'une cellule
-        if self.existe_case_adj():
-            for self.liste2 in self.liste:
-                i = self.liste[0]
-                j = self.liste[1]
-                if i > 0:
-                    if self.plateau[i-1][j] == self.plateau[i][j] and (i-1, j) not in self.liste: #it also checks if the liste2inates is in the list or not
-                        self.liste.append((i-1,j))
-                if i < (size-1):    
-                    if self.plateau[i+1][j] == self.plateau[i][j] and (i+1, j) not in self.liste:
-                        self.liste.append((i+1,j))
-                if j > 0:    
-                    if self.plateau[i][j-1] == self.plateau[i][j] and (i, j-1) not in self.liste:
-                        self.liste.append((i,j-1))
-                if j < (size-1):        
-                    if self.plateau[i][j+1] == self.plateau[i][j] and (i, j+1) not in self.liste:
-                        self.liste.append((i,j+1))
-            return self.liste
+        
+        # Obtenir tous les adjacents d'une cellule et les stocker dans une liste
+        self.liste.append(self.liste2)
+        index = 0
+        i = self.liste2[0]
+        j = self.liste2[1]
+        while(self.adjacent()):
+            if((0 <= i < n) and (0 <= j < n)):
+                tupleToAdd = 0
+                if((j - 1) >= 0):
+                    if(self.plateau[j - 1][i] == self.plateau[j][i]):
+                        tupleToAdd = (i, (j - 1))
+                        if(tupleToAdd not in self.liste):
+                            self.liste.append(tupleToAdd)
+                if((i - 1) >= 0):
+                    if(self.plateau[j][i - 1] == self.plateau[j][i]):
+                        tupleToAdd = ((i - 1), j)
+                        if(tupleToAdd not in self.liste):
+                            self.liste.append(tupleToAdd)
+                if((i + 1) < n):
+                    if(self.plateau[j][i + 1] == self.plateau[j][i]):
+                        tupleToAdd = ((i + 1), j)
+                        if(tupleToAdd not in self.liste):
+                            self.liste.append(tupleToAdd)
+                if((j + 1) < n):
+                    if(self.plateau[j + 1][i] == self.plateau[j][i]):
+                        tupleToAdd = (i, (j + 1))
+                        if(tupleToAdd not in self.liste):
+                            self.liste.append(tupleToAdd)
+            index += 1
+            if(index >= len(self.liste)):
+                print("Liste Envoyée:", self.liste)
+                return self.liste
+
         
 
 
@@ -90,13 +106,13 @@ class game:
         self.plateau[i][j]=+1
         #ici toutes les valeurs des autres liste2onnées dans la liste self.sont mises à 0
         for i in range(1, len(self.liste)):
-            
+            #met un 0 a la case correspondante dans la grille
             self.plateau[self.liste[0]][self.liste[1]] = 0
         
         return self.plateau
             
 
-    def down(self) :            # pour deplacer les cases vers le bas s'il ya des 0 et ajouter de nouvelles valeurs aleatoires
+    def down(self) :     # pour deplacer les cases vers le bas s'il ya des 0 et ajouter de nouvelles valeurs aleatoires
         
         for i in range(size):
             for j in range(size):
@@ -126,30 +142,35 @@ class game:
                         self.plateau[2][j] = self.plateau[i - 4][j]
                         self.plateau[1][j] = self.plateau[i - 5][j]
                         self.plateau[0][j] = 0
-        for p in range(size):
-            for m in range(size):
-                if self.plateau[p][m] == 0:
-                    self.plateau[p][m] = aleatoire(proba) # du coup la derniere case en haut n'aura rien et on y met une valeur aleatoire
+        for x in range(size):
+            for y in range(size):
+                if self.plateau[x][y] == 0:
+                    self.plateau[x][y] = aleatoire(proba) # du coup pour les cases = 0 on y met une valeur aleatoire
         return self.plateau
                     
+    #si le joueur trouvé 10 il gange 
     def found_10(self):
         for i in range(self.size):
             for j in range(self.size):
-                if self.plateau[i][j] >= 10:
+                if self.plateau[i][j] == 10:
                     return True
         return False
 
+    #Calcule Score
     def score(self):
+        self.current_score=1
         for i in range (size):
             for j in range(size):
-                self.current_score =+ self.plateau[i][j]
-                return self.current_score
+               if self.current_score < self.plateau[i][j]:
+                   self.current_score= self.plateau[i][j]
+        return self.current_score
 
+    #Affichage dans le terminale
     def display(self):
         for n in self.plateau:
-            for number in n:
-                print(number, end=' ')
-            print('\n')
+            for x in n:
+                print(x, end=' ')
+            print('')
 
 
 """ La classe de vue GUI du jeu Get10 montrant via tkinter """
@@ -159,7 +180,7 @@ class GUI:
     CELL_PADDING = 1
     BACKGROUND_COLOR = '#f9f6f2'
     SCORE_FONT = "Helvetica", 32, "bold"
-    Couleur1 = {
+    Couleur1 = { #Couleur du Case
         '1': '#2ECC71',
         '2': '#2874A6',
         '3': '#F1C40F',
@@ -172,7 +193,7 @@ class GUI:
         '10': '#5D6D7E',
         'beyond': '#3c3a32'
     }
-    Couleur2 = {
+    Couleur2 = { #Couleur de numéro du case
         '1': '#f9f6f2',
         '2': '#f9f6f2',
         '3': '#f9f6f2',
@@ -190,28 +211,31 @@ class GUI:
 
     def __init__(self, game):
         self.game = game
+        #Initialisation Fenétre tkinter
         self.window = Tk()
+        '''Ouvrir la fenetre, donner un titre, et définir la taille de la fenetre'''
         self.window.title('Just Get 10')
         self.window.resizable(False, False)
-        self.background = Frame(self.window, bg=GUI.BACKGROUND_COLOR)
-        
+        self.frame = Frame(self.window, bg=GUI.BACKGROUND_COLOR)
+        self.frame.config(width=500, height=500)
+        self.frame.grid(row=0, column=0, rowspan=3)
+        self.frame.pack(side=LEFT)
+
         self.case = []
         for i in range(self.game.size):
             ligne = []
             for j in range(self.game.size):
-                label = Label(self.background, text='',
+                label = Label(self.frame, text='',
                                  justify=CENTER, font=GUI.FONT,
                                  width=4, height=2)
-                label.grid(row=i, column=j,padx=1, pady=1)
+                label.grid(row=i, column=j)
                 ligne.append(label)
             self.case.append(ligne)
-        self.background.pack(side=TOP)
+        #event du souris en  clic
         self.window.bind("<Button-1>" , self.Leftclick)
         self.paint()
         self.window.mainloop()
-
-    
-    
+    #Remplir les cases avec les couleurs
     def paint(self):
         for i in range(self.game.size):
             for j in range(self.game.size):
@@ -221,10 +245,7 @@ class GUI:
                     self.case[i][j].configure(
                         text=cell_text,
                         bg=bg_color, fg=fg_color)
-        
-
-
-
+    #Fonction principale du processus de jeu
     def Leftclick(self, event):
         for i in range(self.game.size):
             for j in range(self.game.size):
@@ -236,32 +257,32 @@ class GUI:
         taillex = (self.game.size * GUI.CELL_PADDING )  #calcule la longueur de la grille 
         tailley = (self.game.size *  GUI.CELL_PADDING)    #calcule la hauteur de la grille 
         if x >= 0 and x < taillex and y >= 0 and y < tailley :  #test pour connaitre si on a cliqué a l'interieur de la grille
-            i = int(y / GUI.CELL_PADDING)            # on calcule a partir du y le i de la case selectionnée
-            j = int(x  / GUI.CELL_PADDING)  
+            i = int(y / 8)            # on calcule a partir du y le i de la case selectionnée
+            j = int(x  / 8)  
             current = (i, j)                                 # c'est la case selectionée
-            L = current
-            self.game.propagation(L)
-            print(L) 
-        self.window.update()
-        if self.game.reste_coup() == True : 
-                self.game.update()    #appelle procedure update pour incrementer et mettre les 0
-                self.game.down()     #appelle procedure down
-                self.game.display() #display
-        elif self.game.found_10():
-            return self.you_win()
-        else:
+            C = current
+        if self.game.adjacent() == True : 
+            C = self.game.propagation()
+            print(C) 
+            self.game.update()    #appelle procedure update pour incrementer et mettre les 0
+            self.game.down()     #appelle procedure down
+            self.game.display() #display
+            self.window.update()
+        elif self.game.reste_coup() == False:
             return self.game_over()
-        
+        elif self.game.found_10()== True:
+            return  self.you_win()
+        self.window.update()
         print('Score: {}'.format(self.game.current_score))
         
-    
+    #Win + msg box tkinter
     def you_win(self):
         if not self.won:
             self.won = True
             print('You Win!')
             if messagebox.askyesno('Get10Game', 'You Win!\n' 'Are you going to continue the Get10Game ?'):
                 self.keep_playing = True
-
+    #Game Over + msg box tkinter
     def game_over(self):
         print('Game over!')
         messagebox.showinfo('Get10Game', 'Game over!')
